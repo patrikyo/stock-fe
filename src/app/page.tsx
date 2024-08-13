acbdcf6664084164e1c8771bcab2cfff92c31735
youpa <patrik.youssef@arbetsformedlingen.se>
2024-08-13 09:21:48 +0200

    Adds rudimentary styling and positioning

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,16 @@
 'use client'
-
-import Image from "next/image";
-import styles from "./page.module.css";
+import StockInfo from "./components/StockInfo/StockInfo";
 import useFetch from "./hooks/usefetch";
+import Style from "./page.module.css"
 
 export default function Home() {
     const {data, loading, error} = useFetch("https://stock-api-dh8r.onrender.com/api/stock/SBB-B.ST");
 
 
   return (
-    <main>
+    <main className={Style.mainContent}>
       <h1>börsdata</h1>
-      {data && data.companyName}
-      {data && data.currentPrice}kr
+      { data &&  <StockInfo companyName={data.companyName} currentPrice={data.currentPrice} /> }
     </main>
   );
 }
