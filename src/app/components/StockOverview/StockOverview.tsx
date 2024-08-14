fae14a3770e81f60b4efa3e0a5b373506c13e491
youpa <patrik.youssef@arbetsformedlingen.se>
2024-08-14 10:20:54 +0200

    Adds overview component

---
+++ src/app/components/StockOverview/StockOverview.tsx
@@ -0,0 +1,26 @@
+import useFetch from "@/app/hooks/usefetch";
+import { useEffect } from "react";
+import Style from "./StockOverview.module.css";
+import IStockOverview from "../../interfaces/IStockOverview.interface";
+
+const StockOverview: React.FC<IStockOverview> = ({ticker})=> {
+    const {data, loading, error, getStockInfo} = useFetch();
+    useEffect(()=> {
+      getStockInfo(`https://stock-api-dh8r.onrender.com/api/stock/${ticker}`);
+    }, []);
+
+    return(
+        <div>
+            { loading && <p>Laddar...</p> }
+            { !loading && <ul className={Style.stockDetailList}>            
+                <li>{data && data.companyName}</li>
+                <li>{data && data.currentPrice}</li>
+                <li className={Style.stockDetailItem}><button className={Style.dataFetchBtn} onClick={()=> getStockInfo(`https://stock-api-dh8r.onrender.com/api/stock/${ticker}`)}>hämta börsdata</button></li>
+            </ul>
+            }
+        </div>
+    );
+};
+
+export default StockOverview;
+
---
+++ src/app/interfaces/IStockOverview.interface.tsx
@@ -0,0 +1,5 @@
+interface IStockOverview {
+    ticker: string;
+};
+
+export default IStockOverview;
