dfa0e9026c3294952341dfb0f081bfaae3aca472
youpa <patrik.youssef@arbetsformedlingen.se>
2024-09-21 09:31:00 +0200

    Corrects path

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,28 +1,28 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+  import type { Metadata } from "next";
+  import { Inter } from "next/font/google";
+  import "./globals.css";
 
-import "@fortawesome/fontawesome-svg-core/styles.css";
-import { config } from "@fortawesome/fontawesome-svg-core";
-import Header from "./components/Header/Header";
-config.autoAddCss = false;
+  import "@fortawesome/fontawesome-svg-core/styles.css";
+  import { config } from "@fortawesome/fontawesome-svg-core";
+  import Header from "./components/header/header";
+  config.autoAddCss = false;
 
-const inter = Inter({ subsets: ["latin"] });
+  const inter = Inter({ subsets: ["latin"] });
 
 
-export const metadata: Metadata = {
-  title: "Börsdata",
-  description: "Vissar börsdata för special sit aktier",
-};
+  export const metadata: Metadata = {
+    title: "Börsdata",
+    description: "Vissar börsdata för special sit aktier",
+  };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
-  return (
-    <html lang="en">
-      <body className={inter.className}><Header/>{children}</body>
-    </html>
-  );
-}
+  export default function RootLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <html lang="en">
+        <body className={inter.className}><Header/>{children}</body>
+      </html>
+    );
+  }
