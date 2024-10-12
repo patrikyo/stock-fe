de4f2a6ecff2742accea1adda8e4931ec047844f
youpa <patrik.youssef@arbetsformedlingen.se>
2024-10-12 09:26:58 +0200

    Adds comments and redesigns spinner

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
 import { useEffect, useState } from "react";
 import stockList from "./data/stocks.json"
+import { Spinner } from "react-bootstrap";
 
 export default function Home() {
-  // Använd useState för att hantera din stocks-array
   const [stocks, setStocks] = useState<string[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
 
   useEffect(() => {
     let tempStocks: string[] = [];
@@ -21,25 +22,32 @@ export default function Home() {
         tempStocks.push(stockFromStorage);  
       }
     });
-
     setStocks(tempStocks);
+    setLoading(false);
 
   }, []); 
 
   return (
     <main className={Style.stockContentContainer}>
-      <ul className={Style.stockOverviewList}>
+      {/*  Länk för att lägga till Aktiebolag */}
+      <div className={Style.addStockContainer}>
+        <Link className={Style.addStockLinkContainer} href="/stock-list">
+          <FontAwesomeIcon className={Style.addStockBtn} icon={faPlusCircle} aria-hidden="true"/> 
+          <span>Lägg till börsbolag</span>
+        </Link>
+      </div>
+
+      { loading &&  <div className={Style.loadingIndicatorContainer}><Spinner className={Style.loadingIndicator}/></div>}
+      
+      {/*Lista på börsbolag man följer*/}
+      { !loading && <ul className={Style.stockOverviewList}>
         {stocks.map((ticker: string, index: number) => (
           <li key={index}>
             <StockOverview ticker={ticker} />
           </li>
         ))}
       </ul>
-
-      <div className={Style.addStockLinkContainer}>
-        <FontAwesomeIcon className={Style.addStockBtn} icon={faPlusCircle} />
-        <Link href="/stock-list">Lägg till börsbolag</Link>
-      </div>
+}
     </main>
   );
 }
