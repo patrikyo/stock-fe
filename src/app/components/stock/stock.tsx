9ecd2720cebec2c697e7f62bda59025960f69d1f
youpa <patrik.youssef@arbetsformedlingen.se>
2024-10-13 09:10:48 +0200

    Adds btn to persist stock

--- src/app/components/stock/stock.tsx
+++ src/app/components/stock/stock.tsx
@@ -4,6 +4,10 @@ import Image from "next/image";
 import stocks from "../../data/stocks.json"
 import useFetch from "@/app/hooks/usefetch";
 import { useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlusCircle } from "@fortawesome/free-solid-svg-icons";
+import { Spinner } from "react-bootstrap";
+
 
 const AddStock = ({ticker})=> {
     const {data, loading, error, getStockInfo} = useFetch();
@@ -18,16 +22,17 @@ const AddStock = ({ticker})=> {
     
     return (
         <div>
-            {loading && <p>Laddar....</p>}
+            {loading && <Spinner className={Style.loadingIndicator}/>}
             {!loading && 
             <ul className={Style.stockList}>
                 <li>
                     <h2 className={Style.stockHeader}>{data?.companyName}</h2>
                     <ul className={Style.stockList}>
                         <li><Image src={`/${ticker}.png`} className={Style.stockImage} width={100} height={100} alt="logo"></Image></li>
-                        <li><button onClick={()=> persistStock(ticker)} >Lägg till aktie</button></li>
+                        <li><button className={Style.stockAddBtn} onClick={()=> persistStock(ticker)}><FontAwesomeIcon className={Style.addStockIcon} icon={faPlusCircle} aria-hidden="true"/><span className={Style.addStockCopy}>Lägg till aktie</span></button></li>
                     </ul>
                 </li>
+                <hr className={Style.stockDivider}/>
             </ul>
             }
         </div>
